2b5254c933faba457066288a7393bec6c2f085b1
jonroosevelt <jonathanroosevelt@gmail.com>
2021-11-18 09:16:25 -0300

    wip(home-page-8): first working glitch logo version

--- src/components/layouts/Logo/index.tsx
+++ src/components/layouts/Logo/index.tsx
@@ -6,37 +6,22 @@ import CondensedLogo from "./CondensedLogo";
 import { keyframes, css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-const bounce = keyframes`
-  from, 20%, 53%, 80%, to {
-    transform: translate3d(0,0,0);
-  }
-
-  40%, 43% {
-    transform: translate3d(0, -30px, 0);
-  }
-
-  70% {
-    transform: translate3d(0, -15px, 0);
-  }
-
-  90% {
-    transform: translate3d(0,-4px,0);
-  }
-`;
+const COLOR = "teal";
+const STACKS = 3;
 
 const Logo = (): ReactElement => {
   const fullLogo = useBreakpointValue({ base: false, md: true });
   const logo = (
-    <Box>{fullLogo ? <FullLogo /> : <CondensedLogo fill="teal" />}</Box>
+    <Box>{fullLogo ? <FullLogo /> : <CondensedLogo fill={COLOR} />}</Box>
   );
   return (
     <Container>
       <div style={{ ...styles.stack }}>
-        <Stack>
-          <StackSpan style={{ ...styles.indexes.index0 }}>STACK</StackSpan>
-          <StackSpan style={{ ...styles.indexes.index1 }}>STACK</StackSpan>
-          <StackSpan style={{ ...styles.indexes.index2 }}>STACK</StackSpan>
-        </Stack>
+        <StackSpan style={{ ...styles.stack }}>
+          <span style={{ ...styles.indexes.index0 }}>{logo}</span>
+          <span style={{ ...styles.indexes.index1 }}>{logo}</span>
+          <span style={{ ...styles.indexes.index2 }}>{logo}</span>
+        </StackSpan>
       </div>
     </Container>
   );
@@ -44,19 +29,35 @@ const Logo = (): ReactElement => {
 
 export default Logo;
 
-const COLOR = "teal";
-const STACKS = 3;
 const keyFrameStack = keyframes`
-  0% { opacity: 0; transform: translateX(-50%); textShadow: "-2px 3px 0 red, 2px -3px 0 blue"; }
-  60% { opacity: 0.5; transform: translateX(50%) }
-  80% { transform: none; opacity: 1; textShadow: "2px -3px 0 red, -2px 3px 0 blue"; }
-  100% { textShadow: "none" }
+   0% {
+    opacity: 0;
+    transform: translateX(-50%);
+    filter: drop-shadow(-2px 3px 0 red) drop-shadow(2px -3px 0 blue);
+  };
+  60% {
+    opacity: 0.5;
+    transform: translateX(50%);
+  }
+  80% {
+    transform: none;
+    opacity: 1;
+    filter: drop-shadow(2px -3px 0 red) drop-shadow(-2px 3px 0 blue);
+  }
+  100% {
+    filter: drop-shadow(none);
+  } 
 `;
 
 const keyFrameGlitch = keyframes`
-  0% { textShadow: "-2px 3px 0 red, 2px -3px 0 blue"; transform: translate(var(--glitch-translate)); }
-  2% { textShadow: "2px -3px 0 red, -2px 3px 0 blue" }
-  4%, 100%": { textShadow: "none"; transform: "none" }
+  0% {
+    filter: drop-shadow(-2px 3px 0 red) drop-shadow(2px -3px 0 blue);
+    transform: translate(var(--glitch-translate));
+  }
+  2% {
+    filter: drop-shadow(2px -2px 0 red) drop-shadow(-2px 2px 0 blue);
+  }
+  4%, 100% {  filter: drop-shadow(none); transform: none; }
 `;
 
 const Container = styled.div`
@@ -66,24 +67,21 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const Stack = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-`;
-
-const StackSpan = styled.span`
-  font-weight: bold;
-  grid-row-start: 1;
-  grid-column-start: 1;
-  font-size: 4rem;
-  --stack-height: calc(100% / ${STACKS} - 1px);
-  --inverse-index: calc(calc(${STACKS} - 1) - var(--index));
-  --clip-top: calc(var(--stack-height) * var(--index));
-  --clip-bottom: calc(var(--stack-height) * var(--inverse-index));
-  clip-path: inset(var(--clip-top) 0 var(--clip-bottom) 0);
-  animation-name: ${keyFrameGlitch};
-  animation-duration: 340ms;
-
+const StackSpan = styled.div`
+  span {
+    font-weight: bold;
+    grid-row-start: 1;
+    grid-column-start: 1;
+    font-size: 4rem;
+    --stack-height: calc(100% / ${STACKS} - 1px);
+    --inverse-index: calc(calc(${STACKS} - 1) - var(--index));
+    --clip-top: calc(var(--stack-height) * var(--index));
+    --clip-bottom: calc(var(--stack-height) * var(--inverse-index));
+    clip-path: inset(var(--clip-top) 0 var(--clip-bottom) 0);
+    animation: ${keyFrameStack} 340ms cubic-bezier(0.46, 0.29, 0, 1.24) 1
+        backwards calc(var(--index) * 120ms),
+      ${keyFrameGlitch} 1s ease infinite 1s alternate-reverse;
+  }
   span:nth-child(odd) {
     --glitch-translate: 8px;
   }
@@ -98,8 +96,8 @@ const styles = {
     index1: { "--index": 1 } as React.CSSProperties,
     index2: { "--index": 2 } as React.CSSProperties,
   },
-  stack: { display: "grid", gridTemplateColumns: "1fr" },
-
-  oddStackSpan: { "--glitch-translate": "-8px" },
-  evenStackSpan: { "--glitch-translate": "8px" },
+  stack: {
+    display: "grid",
+    gridTemplateColumns: "1fr",
+  },
 };
