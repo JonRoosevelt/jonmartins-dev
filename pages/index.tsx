5984599a8c2d1fc07d00a3906d0f89d51c8598b7
jonroosevelt <jonathanroosevelt@gmail.com>
2021-11-19 08:34:17 -0300

    feat(home-page-9): implements history on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import React, { ReactElement } from "react";
 import ProfileCard from "../src/components/layouts/IndexPage/ProfileCard";
 import Sup from "../src/components/layouts/IndexPage/Sup";
-
+import History from "../src/components/layouts/IndexPage/History";
 const Page = (): ReactElement => {
   return (
     <>
       <ProfileCard />
       <Sup />
+      <History />
     </>
   );
 };
