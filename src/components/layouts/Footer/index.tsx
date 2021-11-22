a6a167e34a499397d086512fdbd138ee71320fc6
jonroosevelt <jonathanroosevelt@gmail.com>
2021-11-22 10:33:36 -0300

    style(footer): adjusts styling

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -1,12 +1,12 @@
-import { Box, Container, VStack, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { ReactElement } from "react";
 import ResponsiveTable from "../ResponsiveTable";
 import Section from "../Section";
 
 const tableData = [
   { head: "Home", body: ["Sup?", "History", "Stack"] },
-  { head: "Blogs", body: ["Sup?", "History", "Stack"] },
-  { head: "Art", body: ["Sup?", "History", "Stack"] },
+  { head: "Blogs", body: ["Dev", "Design", "Random"] },
+  { head: "Art", body: ["Drawings", "Design"] },
 ];
 
 const Footer = (): ReactElement => {
@@ -14,14 +14,14 @@ const Footer = (): ReactElement => {
     <Section delay={0.8}>
       <Box
         h="100%"
-        mt={20}
+        mt={10}
         bg="rgba(255,255,255,0.1)"
         pt={10}
         pb={20}
         mb={-10}
         align="center"
       >
-        <Container maxW={["container.xs", "container.md"]}>
+        <Container maxW={["container.xs", "container.sm", "container.md"]}>
           <Box paddingX={[4, 0]}>
             <ResponsiveTable tableData={tableData} />
           </Box>
--- src/components/layouts/ResponsiveTable/index.tsx
+++ src/components/layouts/ResponsiveTable/index.tsx
@@ -20,9 +20,9 @@ type Props = {
 const ResponsiveTable = ({ tableData }: Props): ReactElement => {
   return (
     <WrapItems>
-      <Wrap spacing={2} w={["container.xs", "46em"]}>
+      <Wrap spacing={2} w={["container.xs", "container.sm", "46em"]}>
         {tableData.map((column) => (
-          <WrapItem flexWrap={"wrap"} key={column.head}>
+          <WrapItem textAlign="left" flexWrap={"wrap"} key={column.head}>
             <VStack spacing={4} align="stretch" key={column.head}>
               <Box w="container.xs">
                 <Heading color="teal.100" fontSize={18} as="h5" mb="10px">
