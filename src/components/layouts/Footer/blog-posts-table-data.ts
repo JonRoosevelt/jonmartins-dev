daeeccb97cfa65634a15461cd54634c58b3941e8
Jonathan Martins <jonathanroosevelt@gmail.com>
2022-08-18 18:05:42 +0100

    update(blog): I moved to Portugal Pt 1

--- src/components/layouts/Footer/blog-posts-table-data.ts
+++ src/components/layouts/Footer/blog-posts-table-data.ts
@@ -10,6 +10,10 @@ export const blogPostsTableData = [
   {
     head: "Blogs",
     body: [
+      {
+        name: "I moved to Portugal: Part 1",
+        link: "/blog/i-moved-to-portugal-pt1",
+      },
       { name: "Triggers", link: "/blog/triggers" },
       { name: "My First Post", link: "/blog/my-first-post" },
       { name: "See more blog posts...", link: "/blog" },
