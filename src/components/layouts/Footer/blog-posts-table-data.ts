a70ad5f29e93c23395937426bafd514396e406d4
Jonathan Roosevelt Martins <jonathanroosevelt@gmail.com>
2022-09-13 19:11:21 +0100

    update(blog-posts-table-data.ts): adds i moved to portugal pt2 to footer

--- src/components/layouts/Footer/blog-posts-table-data.ts
+++ src/components/layouts/Footer/blog-posts-table-data.ts
@@ -11,11 +11,14 @@ export const blogPostsTableData = [
     head: "Blogs",
     body: [
       {
+        name: "I moved to Portugal: Part 2",
+        link: "/blog/i-moved-to-portugal-pt2",
+      },
+      {
         name: "I moved to Portugal: Part 1",
         link: "/blog/i-moved-to-portugal-pt1",
       },
       { name: "Triggers", link: "/blog/triggers" },
-      { name: "My First Post", link: "/blog/my-first-post" },
       { name: "See more blog posts...", link: "/blog" },
     ],
   },
